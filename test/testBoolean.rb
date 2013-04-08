da642acc1501bd9310fa8da5bc56d343e9c1b2ba
Mark from home <zymurge@gmail.com>
2013-04-08 12:46:51 -0700

    Added custom UT asserts for boolean

---
+++ test/testBoolean.rb
@@ -0,0 +1,8 @@
+  def assert_true( subject, message )
+    assert_equal( subject, true, message )
+  end
+
+  def assert_false( subject, message )
+    assert_equal( subject, false, message )
+  end
+
